055a4ec42e86ab48ecd5c6d84e54876302abe8a6
LeonHo <leon.ho@bchydro.com>
2015-09-10 15:33:28 -0700

    Added student class

---
+++ Student.php
@@ -0,0 +1,40 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * @author Leon
+ */
+class Student {
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    function add_email($witch, $address) {
+        $this->emails[$witch] = $address;
+    }
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value) {
+            $total += $value;
+        }
+        return $total / cout($this->grades);
+    }
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .=  ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what) {
+            $result .= $which . ": " . $what . "\n";
+        }
+        $result .= "\n";
+        return "<pre>" . $result . "</pre>";
+    }
+}
