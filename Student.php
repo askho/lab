73ccfc6e560190e056327c4d28c26ef34d18e371
LeonHo <leon.ho@bchydro.com>
2015-09-10 15:47:21 -0700

    Added some comments to student.php

--- Student.php
+++ Student.php
@@ -1,26 +1,36 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * @author Leon
  */
 class Student {
+    /**
+     * Constructor initializes the fields. 
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
+    /**
+     * Function to add email to a student. 
+     * @param string $witch "The email address that this is related to ex: work"
+     * @param string $address "The actual email address"
+     */
     function add_email($witch, $address) {
         $this->emails[$witch] = $address;
     }
+    /**
+     * Adds a grade to the student. 
+     * @param type $grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
+    /**
+     * Averages the grades
+     * @return int
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value) {
@@ -28,6 +38,10 @@ class Student {
         }
         return $total / count($this->grades);
     }
+    /**
+     * Formats the students nicely and returns it as a string. 
+     * @return string
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .=  ' (' . $this->average() . ")\n";
